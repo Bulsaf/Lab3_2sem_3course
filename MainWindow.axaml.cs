1ea19068a267feec0a4873173faa4cab853f223b
Bulat <bulatsafiullin2612@gmail.com>
2025-03-07 13:09:22 +0300

    final durov

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -22,24 +22,71 @@ public partial class MainWindow : Window
     {
         if (sender is StackPanel panel)
         {
-           
+            var transformGroup = panel.RenderTransform as TransformGroup;
+            var scaleX = transformGroup?.Children[0] as ScaleTransform;
+            var scaleY = transformGroup?.Children[1] as ScaleTransform;
             var shadow = panel.Effect as DropShadowEffect;
 
+            var animation = new Animation
+            {
+                Duration = TimeSpan.FromSeconds(1),
+                Easing = new LinearEasing(),
+                FillMode = FillMode.Forward,
+                IterationCount = IterationCount.Parse("1"),
+                Children =
+                    {
+                        new KeyFrame { Cue = new Cue(0), Setters = { new Setter(ScaleTransform.ScaleXProperty, scaleX?.ScaleX ?? 1) } },
+                        new KeyFrame { Cue = new Cue(1), Setters = { new Setter(ScaleTransform.ScaleXProperty, 1.2) } }
+                    }
+            };
+
+            var animation2 = new Animation
+            {
+                Duration = TimeSpan.FromSeconds(1),
+                Easing = new LinearEasing(),
+                FillMode = FillMode.Forward,
+                IterationCount = IterationCount.Parse("1"),
+                Children =
+                    {
+                        new KeyFrame { Cue = new Cue(0), Setters = { new Setter(ScaleTransform.ScaleYProperty, scaleY?.ScaleY ?? 1) } },
+                        new KeyFrame { Cue = new Cue(1), Setters = { new Setter(ScaleTransform.ScaleYProperty, 1.2) } }
+                    }
+            };
+
+            var animation3 = new Animation
+            {
+                Duration = TimeSpan.FromSeconds(1),
+                Easing = new LinearEasing(),
+                FillMode = FillMode.Forward,
+                IterationCount = IterationCount.Parse("1"),
+                Children =
+                    {
+                        new KeyFrame { Cue = new Cue(0), Setters = { new Setter(DropShadowEffect.OpacityProperty, shadow?.Opacity ?? 0) } },
+                        new KeyFrame { Cue = new Cue(1), Setters = { new Setter(DropShadowEffect.OpacityProperty, 0.6) } }
+                    }
+            };
+
             await Task.WhenAll(
-                AnimationUtils.AnimateDouble(panel, ScaleTransform.ScaleXProperty, 1.2, 1000),
-                AnimationUtils.AnimateDouble(panel, ScaleTransform.ScaleYProperty, 1.2, 1000),
-                AnimationUtils.AnimateDouble(shadow, DropShadowEffect.OpacityProperty, 0.6, 1000)
+                animation.RunAsync(panel),
+                animation2.RunAsync(panel),
+                animation3.RunAsync(shadow)
             );
 
-            // Анимация размытия
-            if (panel.Children[0] is ImageWithArrowsUserControl control)
+            if (panel.Children[0] is ImageWithArrowsUserControl control && control.Effect is BlurEffect blur)
             {
-                await AnimationUtils.AnimateDouble(
-                    control.Effect as BlurEffect,
-                    BlurEffect.RadiusProperty,
-                    0,
-                    1000
-                );
+                var blurAnimation = new Animation
+                {
+                    Duration = TimeSpan.FromSeconds(1),
+                    FillMode = FillMode.Forward,
+                    IterationCount = IterationCount.Parse("1"),
+                    Children =
+                        {
+                            new KeyFrame { Cue = new Cue(0), Setters = { new Setter(BlurEffect.RadiusProperty, blur.Radius) } },
+                            new KeyFrame { Cue = new Cue(1), Setters = { new Setter(BlurEffect.RadiusProperty, 0) } }
+                        }
+                };
+
+                await blurAnimation.RunAsync(blur);
             }
         }
     }
@@ -48,24 +95,70 @@ public partial class MainWindow : Window
     {
         if (sender is StackPanel panel)
         {
-            // Анимация масштаба
+            var transformGroup = panel.RenderTransform as TransformGroup;
+            var scaleX = transformGroup?.Children[0] as ScaleTransform;
+            var scaleY = transformGroup?.Children[1] as ScaleTransform;
             var shadow = panel.Effect as DropShadowEffect;
 
+            var animation = new Animation
+            {
+                Duration = TimeSpan.FromSeconds(2),
+                Easing = new LinearEasing(),
+                IterationCount = IterationCount.Parse("1"),
+                Children =
+                    {
+                        new KeyFrame { Cue = new Cue(0), Setters = { new Setter(ScaleTransform.ScaleXProperty, scaleX?.ScaleX ?? 1.2) } },
+                        new KeyFrame { Cue = new Cue(1), Setters = { new Setter(ScaleTransform.ScaleXProperty, 1) } }
+                    }
+            };
+
+            var animation2 = new Animation
+            {
+                Duration = TimeSpan.FromSeconds(2),
+                Easing = new LinearEasing(),
+                IterationCount = IterationCount.Parse("1"),
+                Children =
+                    {
+                        new KeyFrame { Cue = new Cue(0), Setters = { new Setter(ScaleTransform.ScaleYProperty, scaleY?.ScaleY ?? 1.2) } },
+                        new KeyFrame { Cue = new Cue(1), Setters = { new Setter(ScaleTransform.ScaleYProperty, 1) } }
+                    }
+            };
+
+            var animation3 = new Animation
+            {
+                Duration = TimeSpan.FromSeconds(1),
+                Easing = new LinearEasing(),
+                IterationCount = IterationCount.Parse("1"),
+                Children =
+                    {
+                        new KeyFrame { Cue = new Cue(0), Setters = { new Setter(DropShadowEffect.OpacityProperty, shadow?.Opacity ?? 0.6) } },
+                        new KeyFrame { Cue = new Cue(1), Setters = { new Setter(DropShadowEffect.OpacityProperty, 0) } }
+                    }
+            };
+
             await Task.WhenAll(
-                AnimationUtils.AnimateDouble(panel, ScaleTransform.ScaleXProperty, 1, 2000),
-                AnimationUtils.AnimateDouble(panel, ScaleTransform.ScaleYProperty, 1, 2000),
-                AnimationUtils.AnimateDouble(shadow, DropShadowEffect.OpacityProperty, 0, 1000)
+                animation.RunAsync(panel),
+                animation2.RunAsync(panel),
+                animation3.RunAsync(shadow)
             );
 
-            // Анимация размытия
-            if (panel.Children[0] is ImageWithArrowsUserControl control)
+            if (panel.Children[0] is ImageWithArrowsUserControl control && control.Effect is BlurEffect blur)
             {
-                await AnimationUtils.AnimateDouble(
-                    control.Effect as BlurEffect,
-                    BlurEffect.RadiusProperty,
-                    3,
-                    1000
-                );
+                var blurAnimation = new Animation
+                {
+                    Easing = new LinearEasing(),
+                    
+                    Duration = TimeSpan.FromSeconds(2),
+                    IterationCount = IterationCount.Parse("1"),
+                    Children =
+                        {
+                            new KeyFrame { Cue = new Cue(0), Setters = { new Setter(BlurEffect.RadiusProperty, blur.Radius) } },
+                            new KeyFrame { Cue = new Cue(1), Setters = { new Setter(BlurEffect.RadiusProperty, 3) } }
+                        }
+          
+                };
+
+                await blurAnimation.RunAsync(blur);
             }
         }
     }
